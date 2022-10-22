febeccb6db51c87a1d3a6cd76f317ffa5914cef6
aki hayashi <hayashi1019toy@gmail.com>
2022-10-22 20:52:25 +0900

    modify favorite function by Ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
 
   def create
+    @blog = Blog.find(params[:blog_id])
     current_user.favorites.create(blog_id: params[:blog_id])
     redirect_to blogs_path
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
+    favorite = current_user.favorites.find_by(id: params[:id])
+    @blog = favorite.blog
+    favorite.destroy
     redirect_to blogs_path
   end
-
 end
